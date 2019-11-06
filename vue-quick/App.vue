f3e344d26c569011e8ad19a9d969a0ced2976012
unlessbamboo <unlessbamboo@gmail.com>
2019-11-06 17:57:06 +0800

    跟新

---
+++ vue-quick/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Hello bamboo!</h1>
+</template>
+
+<script>
+/*  export default {
+    name: "App.vue"
+  }*/
+  var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue',
+    }
+  })
+</script>
+
+<style scoped>
+
+</style>
